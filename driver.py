b97d60461f1d9515ffc617af066974cfac23c905
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 13:34:34 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -7,7 +7,8 @@ user_encryption_input = str(raw_input("What is the text? \n")).lower()
 print("\n To generate a new modulus, leave blank. \n")
 generated_modulus = int(raw_input("\nWhat is the Modulus? \n"))
 if generated_modulus == "":
-  key = key_generation.new_mod()
+  upperlimit = int(raw_input("Upper Limmit for Modulus, Must be above 70,000"))
+  key = key_generation.new_mod(upperlimit)
   generated_modulo = key[0]
   print("\n [Encrpytion Key, Decryption Key]  \n"),
   print(key)
