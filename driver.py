b5224b0d41b3caea731afab34f1a42a83d451d7a
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 13:08:58 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ import decrypt
 import encrypt
 import key_generation
 
-user_encryption_input = str(raw_input("How about we encrypt some text? \n")).lower()
-user_encryption_modulo = int(raw_input("\nWith some modulo? \n"))
+user_encryption_input = str(raw_input("What is the text? \n")).lower()
+user_encryption_modulo = int(raw_input("\nWhat is the Modulus? \n"))
 
 print(encrypt.encrypt(user_encryption_input, user_encryption_modulo))
