c58956af2cc23d01025c6e5dbc9492e1862113a5
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 13:14:12 -0700

    Update key_generation.py

--- key_generation.py
+++ key_generation.py
@@ -1,10 +1,12 @@
 from functions import *
 
-largeprimelist = largeprime(random.randint(70000,100000))
-p = largeprimelist[0]*largeprimelist[1] #we do number**e mod p to encrypt
-phip = (largeprimelist[0]-1)*(largeprimelist[1]-1)
-e = 65537
-d = inverse(e,phip)
+def new_mod():
 
+  largeprimelist = largeprime(random.randint(70000,100000))
+  p = largeprimelist[0]*largeprimelist[1] #we do number**e mod p to encrypt
+  phip = (largeprimelist[0]-1)*(largeprimelist[1]-1)
+  e = 65537
+  d = inverse(e,phip)
+  return int(p)
 # print("Encrypt with c = m^65537 mod " + str(p))
 # print("Decrypt with m = c^"+str(d)+" mod "+str(p))
