93d609aa47504ef2d84e48ec1ea1e8ee88ee8c48
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 13:28:47 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -7,5 +7,9 @@ user_encryption_input = str(raw_input("What is the text? \n")).lower()
 print("\n To generate a new modulus, leave blank. \n")
 generated_modulus = int(raw_input("\nWhat is the Modulus? \n"))
 if generated_modulus == "":
-  generated_modulo = key_generation.new_mod()
+  key = key_generation.new_mod()
+  generated_modulo = key[0]
+  print("\n [Encrpytion Key, Decryption Key]  \n"),
+  print(key)
+  
 print(encrypt.encrypt(user_encryption_input, generated_modulo))
