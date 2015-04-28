548ab67d87db28c994a0bce1d360be12e30d98f0
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 12:56:57 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -3,10 +3,6 @@ import decrypt
 import encrypt
 import key_generation
 
-print("yo")
-print("this is a CLI")
-print("Welcom to Seany Wonny's RSA Suite")
-
 user_encryption_input = str(raw_input("How about we encrypt some text? \n")).lower()
 user_encryption_modulo = int(raw_input("\nWith some modulo? \n"))
 
