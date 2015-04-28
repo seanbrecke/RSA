4b8f3b86809e56f7515931c9513dfe32e3c7f4fb
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-28 11:19:13 -0700

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,4 +1,4 @@
-import functions
+from functions import *
 #brute force RSA.
 #The entire security of RSA relies on the fact that it is difficult to factor large primes. The modulus, which is public, if it is able to be factored into its 2 prime components, you can use the totient function, and then the modular inverse to find the decryption key. If we are able to factor the modulus, we know everything.
 
