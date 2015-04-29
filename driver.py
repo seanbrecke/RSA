d2b6956d2230ce0c1abd0c3a041f3d307f65b0c0
Sean Breckenridge <seanbrecke@gmail.com>
2015-04-29 12:55:26 -0700

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,16 +1,15 @@
-import bruteforce
 import decrypt
 import encrypt
 import key_generation
 
 user_encryption_input = str(raw_input("What is the text? \n")).lower()
 print("\n To generate a new modulus, leave blank. \n")
-generated_modulus = int(raw_input("\nWhat is the Modulus? \n"))
-if generated_modulus == "":
-  upperlimit = int(raw_input("Upper Limmit for Modulus, Must be above 70,000"))
-  key = key_generation.new_mod(upperlimit)
-  generated_modulo = key[0]
+generated_modulus = (raw_input("\n What is the Modulus? \n"))
+if generated_modulus == "" or generated_modulus == " ":
+  upperlimit = int(raw_input("\n Upper Limmit for Modulus, Must be above 70,000 \n")),
+  new_key = key_generation.new_mod(upperlimit)
+  generated_modulus = key[0]
   print("\n [Encrpytion Key, Decryption Key]  \n"),
   print(key)
   
-print(encrypt.encrypt(user_encryption_input, generated_modulo))
+print(encrypt.encrypt(user_encryption_input, int(generated_modulus)))
