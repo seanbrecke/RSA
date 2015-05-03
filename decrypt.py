e1c8934d00b5b9054210d57dc96473ec23d283bd
Sean Breckenridge <seanbrecke@gmail.com>
2015-05-03 15:42:22 -0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,25 +1,25 @@
+#Decryption script, padding scheme is just the order in the alphabet, spaces are 27.
 def decrypt(raw_input, decryption_key, modulus_key):
 
 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
-
+#changes variables
 	c = raw_input
 	d = decryption_key
 	m = modulus_key
-
+#uses modulus and decryption key to change encrypted numbers into numbers related to padding scheme
 	n = []
 	for x in c:
-	    n.append(((x**d)%m)-1)
-
+	    n.append(((x**d)%m)-2)
+#changes numbers to letters, it subtracts 2 to make account for adding 2 earlier in encryption.
 	l = []
 	text = ""
 	for x in n:
 	    l.append(alphabet[x])
-
+#changes from a list to a string
 	for x in l:
 	    text += str(x)
-	    
 	return text
-
+#input and output
 user_ciphertext_input = list(raw_input("\n Enter Ciphertext as List \n For example: [15, 41, 53] \n"))
 user_decryption_key_input = int(raw_input("\n What is the Decryption Key \n"))
 user_modulus_input = int(raw_input("\n What is the Modulus? \n"))
