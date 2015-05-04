a529d4c8846ace77c8b900e82daa847cd9e0250d
Sean Breckenridge <seanbrecke@gmail.com>
2015-05-03 20:56:43 -0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,15 +1,14 @@
 #Decryption script, padding scheme is just the order in the alphabet, spaces are 27.
-def decrypt(raw_input, decryption_key, modulus_key):
+def decrypt(r_input, decryption_key, modulus_key):
 
 	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
-#changes variables
-	c = raw_input
-	d = decryption_key
-	m = modulus_key
 #uses modulus and decryption key to change encrypted numbers into numbers related to padding scheme
 	n = []
-	for x in c:
-	    n.append(((x**d)%m)-2)
+	for x in r_input:
+	    if type(x) == int:
+	        long_converted = str((int(x)**decryption_key)%modulus_key)
+	        n.append(int(long_converted[:-1])-2)
+	        
 #changes numbers to letters, it subtracts 2 to make account for adding 2 earlier in encryption.
 	l = []
 	text = ""
